f31a808bac904d9c5736685901732738f2bd7625
Sandeep <107690474+sandeep8750@users.noreply.github.com>
2025-11-02 21:37:25 +0530

    added newlog

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -18,23 +18,19 @@ export function Navigation() {
   ]
 
   return (
-    <nav className="fixed top-0 w-full bg-background/70 backdrop-blur-md border-b border-border z-50">
+    <nav className="fixed top-0 w-full  bg-background/70 backdrop-blur-md border-b border-border z-50 p-1">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
 
           {/* Logo */}
-            <Link href="/" className="flex items-center gap-2 hover:opacity-90 transition">
+         <Link href="/" className="flex items-center hover:opacity-90 transition">
               <img
-                src="/logo7.png"
+                src="/newlogo.png"
                 alt="Vedantix logo"
-                width={120}
-                height={40}
-                className="object-contain"
+                className="w-[120px] h-[60px] object-contain"
               />
-              {/* <span className="text-xl font-bold text-primary">
-                Vedantix
-              </span> */}
-            </Link>
+          </Link>
+
 
 
 
